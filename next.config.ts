8f73c8e9ec883b02fa5ed683418cd9cf1edd27cf
alexiva-85 <IVASHCHENKO85@ICLOUD.COM>
2025-12-30 08:18:23 +0700

    Revert "fix: redirect no-slash routes"
    This reverts commit be0c2b09917d9d80e6b1f24b5c58d36281b1093f.

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: "/cart",
-        destination: "/cart/",
-        permanent: true,
-      },
-      {
-        source: "/checkout",
-        destination: "/checkout/",
-        permanent: true,
-      },
-      {
-        source: "/catalog",
-        destination: "/catalog/",
-        permanent: true,
-      },
-      {
-        source: "/catalog/:gameSlug",
-        destination: "/catalog/:gameSlug/",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
